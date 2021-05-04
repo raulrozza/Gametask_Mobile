ddcf8289c92bbbef728c2d21f93a0fb377f25daf
Raul <raul.hmetal@gmail.com>
2021-05-04 15:22:03 -0300

    Add: Get Current Leaderboard service

---
+++ src/modules/selectedGame/services/factories/makeGetCurrentLeaderboard.ts
@@ -0,0 +1,8 @@
+import makeLeaderboardsRepository from 'modules/selectedGame/repositories/factories/makeLeaderboardsRepository';
+import GetCurrentLeaderboard from 'modules/selectedGame/services/GetCurrentLeaderboard';
+
+export default function makeGetCurrentLeaderboard(): GetCurrentLeaderboard {
+  const repository = makeLeaderboardsRepository();
+
+  return new GetCurrentLeaderboard(repository);
+}
