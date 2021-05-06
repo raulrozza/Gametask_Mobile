c33b0cef7a08eee4804489c95f266d76f29cd134
Raul <raul.hmetal@gmail.com>
2021-05-06 14:19:51 -0300

    Edit: Added find player service

---
+++ src/modules/selectedGame/services/FindPlayerService.ts
@@ -0,0 +1,25 @@
+import IPlayersRepository from 'modules/selectedGame/repositories/IPlayersRepository';
+import IPlayer from 'shared/entities/IPlayer';
+
+interface IExecute {
+  player?: IPlayer;
+  shouldLogout?: boolean;
+  error?: string;
+}
+
+export default class FindPlayerService {
+  constructor(private playersRepository: IPlayersRepository) {}
+
+  public async execute(id: string): Promise<IExecute> {
+    try {
+      const player = await this.playersRepository.findById(id);
+
+      return { player };
+    } catch (error) {
+      return {
+        error: error.message,
+        shouldLogout: error.shouldLogout,
+      };
+    }
+  }
+}
