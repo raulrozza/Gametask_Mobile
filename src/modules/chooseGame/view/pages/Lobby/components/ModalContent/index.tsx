0d7fc062b4a8e2527117803167505baceafbbe1a
Raul <raul.hmetal@gmail.com>
2021-08-11 10:45:36 -0300

    Edit: Reading QR Code to accept invite

--- src/modules/chooseGame/view/pages/Lobby/components/ModalContent/index.tsx
+++ src/modules/chooseGame/view/pages/Lobby/components/ModalContent/index.tsx
@@ -3,16 +3,26 @@ import React, { useCallback, useMemo, useState } from 'react';
 // Components
 import Clipboard from 'expo-clipboard';
 import Button from 'shared/view/components/Button';
+import { BarCodeScannedCallback } from 'expo-barcode-scanner';
 
 // Hooks
 import { useNavigation } from '@react-navigation/native';
+import { useCodeScannerPermission } from './hooks';
 import useToastContext from 'shared/container/contexts/ToastContext/contexts/useToastContext';
 
 // Providers
 import { makeCryptoProvider } from 'modules/chooseGame/container/providers';
 
 // Styles
-import { Container, Wrapper, PasteGroup, PageTitle } from './styles';
+import {
+  Container,
+  Wrapper,
+  PasteGroup,
+  PageTitle,
+  ScannerContainer,
+  CodeScanner,
+} from './styles';
+import { Modal } from 'react-native';
 
 interface IInvitationData {
   gameId: string;
@@ -23,30 +33,63 @@ interface ModalContentProps {
   closeModal(): void;
 }
 
+const QRCODE_TYPE = 256;
+
 const ModalContent: React.FC<ModalContentProps> = ({ closeModal }) => {
   const [code, setCode] = useState('');
   const [inviteData, setInviteData] = useState<IInvitationData | null>(null);
+
+  const scannerPermission = useCodeScannerPermission();
+  const [showScanner, setShowScanner] = useState(false);
 
   const { navigate } = useNavigation();
   const toast = useToastContext();
 
   const crypto = useMemo(() => makeCryptoProvider(), []);
 
+  const decryptCode = useCallback(
+    (encrypted: string): IInvitationData | undefined => {
+      const decrypted = crypto.decrypt<IInvitationData>(encrypted);
+
+      if (!decrypted || !decrypted.gameId || !decrypted.inviter) {
+        toast.showError('Código de jogo inválido.');
+
+        return undefined;
+      }
+
+      return decrypted;
+    },
+    [crypto, toast],
+  );
+
+  const handleCodeScan = useCallback<BarCodeScannedCallback>(
+    ({ data, type }) => {
+      if (Number(type) !== QRCODE_TYPE) return;
+
+      setShowScanner(false);
+
+      const inviteData = decryptCode(data);
+
+      if (!inviteData) return setInviteData(null);
+
+      setCode(data);
+
+      return setInviteData(inviteData);
+    },
+    [decryptCode],
+  );
+
   const handleCodePaste = useCallback(async () => {
     const clipboardText = await Clipboard.getStringAsync();
 
-    const decrypted = crypto.decrypt<IInvitationData>(clipboardText);
+    const inviteData = decryptCode(clipboardText);
 
-    if (!decrypted || !decrypted.gameId || !decrypted.inviter) {
-      toast.showError('Código de jogo inválido.');
-      setCode('');
-      return setInviteData(null);
-    }
+    if (!inviteData) return setInviteData(null);
 
     setCode(clipboardText);
 
-    return setInviteData(decrypted);
-  }, [crypto, toast]);
+    return setInviteData(inviteData);
+  }, [decryptCode]);
 
   const handleSubmitInvitation = useCallback(async () => {
     if (code === '' || !inviteData) return;
@@ -75,6 +118,26 @@ const ModalContent: React.FC<ModalContentProps> = ({ closeModal }) => {
             <PasteGroup.Icon name="content-paste" />
           </PasteGroup.Button>
         </PasteGroup.Container>
+
+        {scannerPermission === 'granted' && (
+          <ScannerContainer>
+            <Modal
+              visible={showScanner}
+              statusBarTranslucent={true}
+              supportedOrientations={['portrait']}
+            >
+              <CodeScanner onBarCodeScanned={handleCodeScan} />
+            </Modal>
+
+            <Button
+              outline
+              onPress={() => setShowScanner(true)}
+              type="touchable-opacity"
+            >
+              Ler Código QR
+            </Button>
+          </ScannerContainer>
+        )}
 
         <Button onPress={handleSubmitInvitation} type="touchable-opacity">
           ENTRAR
--- src/modules/chooseGame/view/pages/Lobby/components/ModalContent/styles.ts
+++ src/modules/chooseGame/view/pages/Lobby/components/ModalContent/styles.ts
@@ -2,6 +2,8 @@ import styled, { css } from 'styled-components/native';
 
 // Components
 import { MaterialIcons } from '@expo/vector-icons';
+import { Dimensions } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
 
 export const Wrapper = styled.View`
   flex: 1;
@@ -62,3 +64,24 @@ export const PasteGroup = {
     color: ${({ theme }) => theme.palette.secondary.main};
   `,
 };
+
+export const ScannerContainer = styled.View`
+  ${({ theme }) => css`
+    margin-bottom: ${theme.layout.spacing(4)};
+  `}
+`;
+
+export const CodeScanner = styled(BarCodeScanner)`
+  ${({ theme }) => {
+    const { width, height } = Dimensions.get('window');
+
+    return css`
+      width: ${width}px;
+      height: ${height}px;
+
+      border-radius: ${theme.layout.borderRadius.medium};
+
+      border: 1px solid ${theme.palette.secondary.main};
+    `;
+  }}
+`;
